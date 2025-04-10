b9677a871587883651c164175c0a79049907ef1f
Thiranjaya Siriwardhana <kanujathiranjaya@gmail.com>
2025-04-10 13:00:07 +0530

    Started backend

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
+})
+
+app.listen(5000, () =>{
+    console.log(`Server is running...`);
+})
