d0cb2019b0b48cd9f000fc99a49a65d3f0f3c95c
Thiranjaya Siriwardhana <kanujathiranjaya@gmail.com>
2025-04-24 08:19:40 +0530

    I started to create cloud db

--- server/config/db.js
+++ server/config/db.js
@@ -8,7 +8,7 @@ export const connectDB = async () => {
         // Use 127.0.0.1 instead of localhost to avoid IPv6 issues
         // const mongoURI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/Buvons";
         
-        const conn = await mongoose.connect("mongodb://127.0.0.1:27017/Buvons");
+        const conn = await mongoose.connect("");
         
         console.log(`MongoDB Connected: ${conn.connection.host}`);
         return conn;
