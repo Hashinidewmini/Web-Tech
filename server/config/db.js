d11bfd7a2e6b596dfb2249ae70929f0d2f1f863c
Thiranjaya Siriwardhana <kanujathiranjaya@gmail.com>
2025-04-25 09:09:23 +0530

    Coonected to the cloud mongodb

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,9 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
 
-dotenv.config();
-
-export const connectDB = async () => {
+export const connectDB = async () => { 
     try {
-        // Use 127.0.0.1 instead of localhost to avoid IPv6 issues
-        // const mongoURI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/Buvons";
         
-        const conn = await mongoose.connect("");
+        const conn = await mongoose.connect(process.env.MONGO_URI); 
         
         console.log(`MongoDB Connected: ${conn.connection.host}`);
         return conn;
